dc885dbfb2012d8be375a0d3b513f6105f8a47f0
Hadley <hadley_dalton@brown.edu>
2021-05-22 21:42:57 -0400

    COMPLETELY DONE

--- src/stores/IframeNodeStore.ts
+++ src/stores/IframeNodeStore.ts
@@ -5,6 +5,7 @@ export class IframeNodeStore extends NodeStore {
     /*Set upon instantiation and determines whether the Store is nested inside a collection or not. */
     public notNested;
 
+    /*Initializes the node's size and location. */
     constructor(initializer: Partial<IframeNodeStore>) {
         super();
         Object.assign(this, initializer);
--- src/stores/ImageNodeStore.ts
+++ src/stores/ImageNodeStore.ts
@@ -5,6 +5,7 @@ export class ImageNodeStore extends NodeStore {
     /*Set upon instantiation and determines whether the Store is nested inside a collection or not. */
     public notNested;
 
+    /*Initializes the node's size and location. */
     constructor(initializer: Partial<ImageNodeStore>) {
         super();
         Object.assign(this, initializer);
